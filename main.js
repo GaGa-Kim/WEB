3b6355b479fe8c98adfbb0e59d95b582e0f014bf
GaGa <87821678+GaGa-Kim@users.noreply.github.com>
2021-08-08 00:32:17 +0900

    Nodejs-mysql Study 1
    2021.08.06

--- main.js
+++ main.js
@@ -5,13 +5,24 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host     : '',
+  port     : 3307,  
+  user     : '',
+  password : '',
+  database : ''
+});
+db.connect();
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
+    // 최상위 경로 - 처음 부분
     if(pathname === '/'){
       if(queryData.id === undefined){
+        /* 파일을 읽어와서 목록 보여주기
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
@@ -22,8 +33,23 @@ var app = http.createServer(function(request,response){
           );
           response.writeHead(200);
           response.end(html);
+        }); */
+
+        // mysql을 읽어와서 목록 보여주기
+        db.query(`SELECT * FROM topic`, function(err, topics){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
         });
       } else {
+        // 상세보기
+        /* 파일을 읽어와서 상세보기 보여주기
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -45,9 +71,36 @@ var app = http.createServer(function(request,response){
             response.writeHead(200);
             response.end(html);
           });
+        }); */
+
+        // mysql을 읽어와서 상세보기 보여주기
+        db.query(`SELECT * FROM topic`, function(err, topics){
+          if(err) {
+            throw err;  // 에러 발생 시 콘솔 출력 및 애플리케이션 중지
+          }
+          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(err2, topic) {
+            if(err2) {
+              throw err2;  
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>
+              <a href="/update?id=${queryData.id}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${queryData.id}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          })
         });
       }
     } else if(pathname === '/create'){
+      /* 글 생성하기
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
@@ -64,6 +117,24 @@ var app = http.createServer(function(request,response){
         `, '');
         response.writeHead(200);
         response.end(html);
+      }); */
+      db.query(`SELECT * FROM topic`, function(err, topics){
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `<form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>`,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
       });
     } else if(pathname === '/create_process'){
       var body = '';
@@ -72,14 +143,29 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
+          /* 글 생성하기
           var title = post.title;
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
-          })
+          }) */
+
+          db.query(`
+            INSERT INTO topic (title, description, created, author_id) 
+              VALUES(?, ?, NOW(), ?)`, 
+              [post.title, post.description, 1], 
+              function(err, result) {
+                if(err) {
+                 throw err;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+           )
       });
     } else if(pathname === '/update'){
+      /* 글 수정하기
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -103,7 +189,36 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(html);
         });
-      });
+      }); */
+
+       db.query(`SELECT * FROM topic`, function(err, topics){
+          if(err) {
+            throw err;  
+          }
+          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(err2, topic) {
+            if(err2) {
+              throw err2;  
+            }
+            var list = template.list(topics);
+            var html = template.HTML(topic[0].title, list,
+              `
+              <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          });
+        });
     } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
@@ -111,6 +226,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
+          /* 글수정하기
           var id = post.id;
           var title = post.title;
           var description = post.description;
@@ -119,7 +235,13 @@ var app = http.createServer(function(request,response){
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
             })
-          });
+          }); */
+          db.query(`
+          UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, 
+          [post.title, post.description, post.id], function(err, result) {
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
+          })
       });
     } else if(pathname === '/delete_process'){
       var body = '';
@@ -128,12 +250,20 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
+          db.query(`DELETE FROM topic WHERE id=?`, [post.id], function(err, result) {
+            if(err) {
+              throw err;
+            }
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          });
+          /* 글 삭제하기
           var id = post.id;
           var filteredId = path.parse(id).base;
           fs.unlink(`data/${filteredId}`, function(error){
             response.writeHead(302, {Location: `/`});
             response.end();
-          })
+          }) */
       });
     } else {
       response.writeHead(404);
