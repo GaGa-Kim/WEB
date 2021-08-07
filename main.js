de2463f20ebddcb968ce9d5106bc805afc005f37
GaGa <87821678+GaGa-Kim@users.noreply.github.com>
2021-08-08 00:36:45 +0900

    Nodejs-mysql Study 2
    2021.08.07

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    // 최상위 경로 - 처음 부분
     if(pathname === '/'){
       if(queryData.id === undefined){
         /* 파일을 읽어와서 목록 보여주기
@@ -48,7 +47,6 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        // 상세보기
         /* 파일을 읽어와서 상세보기 보여주기
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
@@ -73,12 +71,12 @@ var app = http.createServer(function(request,response){
           });
         }); */
 
-        // mysql을 읽어와서 상세보기 보여주기
+        // mysql을 읽어와서 상세보기 보여주기 + join을 이용해서 저자 출력
         db.query(`SELECT * FROM topic`, function(err, topics){
           if(err) {
             throw err;  // 에러 발생 시 콘솔 출력 및 애플리케이션 중지
           }
-          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(err2, topic) {
+          db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function(err2, topic) {
             if(err2) {
               throw err2;  
             }
@@ -86,7 +84,7 @@ var app = http.createServer(function(request,response){
             var description = topic[0].description;
             var list = template.list(topics);
             var html = template.HTML(title, list,
-              `<h2>${title}</h2>${description}`,
+              `<h2>${title}</h2>${description} <p>by ${topic[0].name}</p>`,
               `<a href="/create">create</a>
               <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
@@ -100,7 +98,7 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      /* 글 생성하기
+      /* 파일을 읽어와서 글 생성하기 
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
@@ -118,23 +116,36 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(html);
       }); */
+
+      // mysql을 읽어와서 글 생성하기 + join을 이용해서 저자 적용
       db.query(`SELECT * FROM topic`, function(err, topics){
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-          `<form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
+        db.query(`SELECT * FROM author`, function(err2, authors) {
+          /* template.js로 빼내기 - authorSelect
+          var tag = '';
+          var i = 0;
+          while(i < authors.length) {
+            tag += `<option value="${authors[i].id}">${authors[i].name}</option>`
+            i++;
+          } */
+          var title = 'Create';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `<form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+            ${template.authorSelect(authors)}
+            <p>
+              <input type="submit">
+            </p>
+          </form>`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
     } else if(pathname === '/create_process'){
       var body = '';
@@ -143,7 +154,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          /* 글 생성하기
+          /* 파일을 읽어와서 글 생성하기
           var title = post.title;
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
@@ -151,10 +162,11 @@ var app = http.createServer(function(request,response){
             response.end();
           }) */
 
+          // mysql을 읽어와서 글 생성하기 + join을 이용해서 저자 선택하기
           db.query(`
             INSERT INTO topic (title, description, created, author_id) 
               VALUES(?, ?, NOW(), ?)`, 
-              [post.title, post.description, 1], 
+              [post.title, post.description, post.author], 
               function(err, result) {
                 if(err) {
                  throw err;
@@ -165,7 +177,7 @@ var app = http.createServer(function(request,response){
            )
       });
     } else if(pathname === '/update'){
-      /* 글 수정하기
+      /* 파일을 읽어와서 글 수정하기
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -191,6 +203,7 @@ var app = http.createServer(function(request,response){
         });
       }); */
 
+      // mysql을 읽어와서 글 수정하기 + join을 이용해서 저자 출력
        db.query(`SELECT * FROM topic`, function(err, topics){
           if(err) {
             throw err;  
@@ -199,24 +212,29 @@ var app = http.createServer(function(request,response){
             if(err2) {
               throw err2;  
             }
-            var list = template.list(topics);
-            var html = template.HTML(topic[0].title, list,
-              `
-              <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>
-              `,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            response.writeHead(200);
-            response.end(html);
+            db.query(`SELECT * FROM author`, function(err2, authors) {
+              var list = template.list(topics);
+              var html = template.HTML(topic[0].title, list,
+                `
+                <form action="/update_process" method="post">
+                  <input type="hidden" name="id" value="${topic[0].id}">
+                  <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                  <p>
+                    <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                  </p>
+                  <p>
+                  ${template.authorSelect(authors, topic[0].author_id)}
+                  <p>
+                  <p>
+                    <input type="submit">
+                  </p>
+                </form>
+                `,
+                `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+              );
+              response.writeHead(200);
+              response.end(html);
+            });
           });
         });
     } else if(pathname === '/update_process'){
@@ -226,7 +244,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          /* 글수정하기
+          /* 파일을 읽어와서 글수정하기
           var id = post.id;
           var title = post.title;
           var description = post.description;
@@ -236,9 +254,11 @@ var app = http.createServer(function(request,response){
               response.end();
             })
           }); */
+
+          // mysql을 읽어와서 글 수정하기 + join을 이용해서 저자 수정
           db.query(`
-          UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, 
-          [post.title, post.description, post.id], function(err, result) {
+          UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, 
+          [post.title, post.description, post.author, post.id], function(err, result) { // <select name="author"> 이므로 post.author
             response.writeHead(302, {Location: `/?id=${post.id}`});
             response.end();
           })
@@ -250,6 +270,7 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
+          // // mysql을 읽어와서 글 삭제하기
           db.query(`DELETE FROM topic WHERE id=?`, [post.id], function(err, result) {
             if(err) {
               throw err;
@@ -257,7 +278,7 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {Location: `/`});
             response.end();
           });
-          /* 글 삭제하기
+          /* 파일을 읽어와서 글 삭제하기
           var id = post.id;
           var filteredId = path.parse(id).base;
           fs.unlink(`data/${filteredId}`, function(error){
